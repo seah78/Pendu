52240ea6f28bf17d6a2f771db9953ec279bfcfbc
seah78 <github@seah.pro>
2021-05-05 07:36:07 +0200

    Partie 1

--- models/game.py
+++ models/game.py
@@ -1,2 +1,27 @@
-class Game:
+class HangmanGame:
 	pass
+
+class Game:
+
+	def __init__(self):
+		self.ongoing = None
+		self.n_propositions = None
+		self.user = None
+
+	def save(self):
+		pass
+
+	def start(self):
+		pass
+
+	def restart(self):
+		pass
+
+	def give_up(self):
+		pass
+
+	def is_active(self):
+		pass
+
+	def is_won(self):
+		pass
--- models/words.py
+++ models/words.py
@@ -1,2 +1,14 @@
 class SecretWord:
-	pass
+	
+	def __init__(self):
+		self.length = None
+		self.n_hidden_letter = None
+		self.n_visible_letters = None
+
+	def propose_letter(self, letter):
+
+		pass
+
+	def is_found(self):
+
+		pass
