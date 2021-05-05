5d35e6831bc2607d33953c404e400431c4d64a14
seah78 <githubseah.pro>
2021-05-05 17:58:29 +0200

    Passage sur portable

---
+++ controllers/__init__.py
@@ -0,0 +1,22 @@
+class ApplicationController:
+
+	def __init__(self):
+		pass
+
+	def start(self):
+		self.running = True
+		while self.running:
+			# choix <- Afficher le menu et demander un chois à l'utilisateur
+			# si le choix est de se connecter:
+			# ...
+
+
+class MainMenuController:
+	pass
+
+class ConnectionMenuController:
+	pass
+
+class SignupMenuController:
+	pass
+	
