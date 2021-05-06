c409f7fd4d62460f4a9a5c8b2e6e96a3063710cd
seah78 <githubseah.pro>
2021-05-06 18:44:46 +0200

    Webinaire 51 mn

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,22 +1,61 @@
+from ..views import  HomeMenuView
+from ..models import Menu
+
+
 class ApplicationController:
 
 	def __init__(self):
-		pass
+		self.controller = None
+
 
 	def start(self):
-		self.running = True
-		while self.running:
-			# choix <- Afficher le menu et demander un chois à l'utilisateur
-			# si le choix est de se connecter:
-			# ...
+		self.controller = HomeMenuController
+		while self.controller:
+			self.controller = self.controller()
 
+	
 
-class MainMenuController:
-	pass
+class HomeMenuController:
+	def __init__(self):
+		self.menu = Menu()
+		self.view = HomeMenuView()
+
+	def __call__(self):
+		# 1. Construire un menu
+		self.menu.add("auto", "Se connecter", ConnectionMenuController())
+		self.menu.add("auto", "Commencer une partie", NewGameController())
+		self.menu.add("q", "Quitter", EndScreenController())
+
+		# 2. Demander à la vue d'afficher le menu et de collecter la réponse de l'utilisateur
+		user_choice = self.view.get_user_choice()
+
+		# 3. Retourner le controller associé au chois de l'utilisateur au contrôleur principal
+		return user_choice.controller
 
 class ConnectionMenuController:
-	pass
+	def __call__(self):
+		return # controlleur suivant
 
 class SignupMenuController:
 	pass
+
+class  NewGameController:
+	pass
+
+
+class OnGoingGameController:
+	pass
+
+class RankingController:
+
+	pass
+
+class EndScreenController:
+	pass
+
+
+if __name__ == "__main__":
+	app = ApplicationController()
+	app.start()
+
 	
